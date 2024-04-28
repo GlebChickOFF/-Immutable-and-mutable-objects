8756ef524a114444ee1f78e621300379efeabfe9
GlebChickOFF <167457773+GlebChickOFF@users.noreply.github.com>
2024-04-28 18:48:22 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+immutable_var = 1 , 2, 'a', 'b'
+print(immutable_var)
+# immutable_var[0] = 3
+# print(immutable_var)
+# кортеж - не изменяемая коллекция
+mutable_list = [1, 2, 'a', 'b']
+print(mutable_list)
+mutable_list.append('c')
+print(mutable_list)
